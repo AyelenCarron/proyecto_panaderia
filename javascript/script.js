9124900463af2707ade62a807b434cbe25a33188
Ayelen ITSC <mercedes.carron@itscordoba.edu.ar>
2025-06-20 09:15:23 -0300

    Pagina preliminar

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const selectOrigen = document.getElementById ("selectOrigen");
+const bloqueOtro = document.getElementById ("bloqueOtro");
+const contactForm = document.getElementById('contactForm');
+
+selectOrigen.addEventListener("change",function(){
+    const op= this.value;
+    if(op == "otro"){
+        bloqueOtro.classList.remove("escondido");
+    }else if(op == "otro")
+        bloqueOtro.classList.add("escondido");
+})
+
+contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        alert('Mensaje enviado');
+
+        
+        contactForm.reset();
+    });
